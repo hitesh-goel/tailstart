343cb8aa99cfb698af7307e9f1ec6ff324c501c4
Diego Polido <diego@polidosantana.com.br>
2024-04-22 10:29:58 -0300

    Typing App Name on Shell Script

--- create.sh
+++ create.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
-mkdir myapp && cd myapp 
+echo -n "Enter Rails App name (i.e.: my_app): " 
+read myapp
+mkdir -p "generated_app/$myapp" && cd "generated_app/$myapp" 
 echo "source 'https://rubygems.org'" > Gemfile
 echo "gem 'rails', '7.1.3'" >> Gemfile
 bundle install
 
 # https://github.com/rails/cssbundling-rails/issues/146 
-echo "Y" | bundle exec rails new . --force --database=postgresql -j=esbuild -c=tailwind -m=../template.rb --skip-docker
+echo "Y" | bundle exec rails new . --force --database=postgresql -j=esbuild -c=tailwind -m=../../template.rb --skip-docker
 
 
